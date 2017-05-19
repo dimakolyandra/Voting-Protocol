ce14781946e3aec4a57e5bd81301c730ebcc6ee0
Дмитрий Коляндра <dima.kolyandra@mail.ru>
2017-05-19 13:55:38 +0300

    Corrected registration

--- Client.py
+++ Client.py
@@ -1,6 +1,8 @@
 import zmq
 import pickle
 import RunningCommandException as myexc
+from Crypto import Random
+from Crypto.Cipher import AES
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
@@ -13,6 +15,7 @@ class ClientVote:
         self.socket.connect(port)
         self.client_private_key = RSA.generate(2048)
         self.server_open_key = ""
+        self.id = -1
         f = open('client_private_key.txt','w')
         f.write(str(self.client_private_key.exportKey()))
         f.close()
@@ -22,33 +25,56 @@ class ClientVote:
         f.close()
 
     def send_msg(self, *msg):
+        """ Отправить сообщение """
         self.socket.send(pickle.dumps(msg))
         ans = pickle.loads(self.socket.recv())
         return ans
 
-    def encrypt_msg(self, msg):
-        encr_msg = self.encrypt_obj.encrypt(msg.encode(),0)
-        return encr_msg
+    def encrypt_msg(self, msg, session_key):
+        """ Зашифровать сообщение"""
+        iv = Random.new().read(16)
+        obj = AES.new(session_key, AES.MODE_CFB, iv)
+        encrypted_msg = iv + obj.encrypt(msg)
+        encr_session_key = self.encrypt_obj.encrypt(session_key, 0)
+        return encr_session_key, encrypted_msg
 
-    def register_request(self):
-        d, self.server_open_key = self.send_msg('register_request', '')
-        self.encrypt_obj = RSA.importKey(self.server_open_key.exportKey())
-        sort_dict = sorted(d.keys())
-        for key in sort_dict:
-            print(key, end=": ")
-            need_data = input()
-            d[key] = self.encrypt_msg(need_data)
-        ans = self.send_msg('finish_register', d,self.client_public_key)
-        if ans != 'ok':
-            raise myexc.RunningCommandException()
+    def register_request(self, session_key):
+        """ Запрос на решистрацию"""
+        d, self.server_open_key = self.send_msg('register_request', '', '-1', '-1', self.id)
+        if d == -1:
+            print('You already have been registered!')
+        else:
+            self.encrypt_obj = RSA.importKey(self.server_open_key.exportKey())
+            sort_dict = sorted(d.keys())
+            for key in sort_dict:
+                print(key, end=": ")
+                need_data = input()
+                encr_session_key, d[key] = self.encrypt_msg(need_data, session_key)
+            ans, id = self.send_msg('finish_register', d, encr_session_key, self.client_public_key,'-1')
+            if ans != 'ok':
+                raise myexc.RunningCommandException()
+            else:
+                self.id = id
+
+    def begin_voting(self, session_key):
+
+        self.send_msg('begin_voting','',)
+
 
     def go(self):
-        try:
-            self.register_request()
-        except myexc.RunningCommandException:
-            print(myexc.RunningCommandException.text)
+        """ Основной цикл работы"""
+        while True:
+            try:
+                session_key = Random.new().read(32)
+                cmd = input()
+                if cmd == "-r":
+                    self.register_request(session_key)
+                if cmd == "-v":
+                    self.begin_voting(session_key)
+            except myexc.RunningCommandException:
+                print(myexc.RunningCommandException.text)
 
 
 if __name__ == "__main__":
-    client = ClientVote('tcp://127.0.0.1:9090')
+    client = ClientVote('tcp://127.0.0.1:8090')
     client.go()
--- Server.py
+++ Server.py
@@ -1,19 +1,15 @@
 import zmq
 import pickle
 from Crypto.PublicKey import RSA
-import random
+from Crypto.Cipher import AES
+from Crypto import Random
 
 class ServerVote:
-
-    """ Класс, реализующий голосование """
-
     def __init__(self):
-
         """ Начальная инициализация """
-
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REP)
-        self.data_of_constituency = [{'id':'','data':{}}]
+        self.data_of_constituency = []
         self.server_private_key = RSA.generate(2048)
         self.decryption_obj = RSA.importKey(self.server_private_key.exportKey())
         f = open('server_private_key.txt', 'w')
@@ -24,41 +20,59 @@ class ServerVote:
         f.write(str(self.server_public_key.exportKey()))
         f.close()
 
-    #def register_in_base(self,d):
-    #    id = random.random(0,100)
-    #    dictionary = {'id': '', 'first_name': '', 'second_name': '', 'open_key': ''}
-    #    if dictionary['id'] != '':
+    def register_in_base(self, d):
+        """ Зарегистрировать в базе """
+        dictionary = {'id': '', 'first_name': '', 'second_name': '', 'open_key': ''}
+        id = len(self.data_of_constituency) + 1
+        dictionary['id'] = id
+        dictionary['first_name'] = d['first_name']
+        dictionary['second_name'] = d['second_name']
+        dictionary['open_key'] = d['open_key']
+        self.data_of_constituency.append(dictionary)
+        return id
 
+    #def encrypt_msg(self, msg, session_key, id):
+    #    """ Зашифровать сообщение"""
+    #    encrypt_obj = RSA.importKey(self.server_open_key.exportKey())
+    #    iv = Random.new().read(16)
+    #    obj = AES.new(session_key, AES.MODE_CFB, iv)
+    #    encrypted_msg = iv + obj.encrypt(msg)
+    #    encr_session_key = encrypt_obj.encrypt(session_key, 0)
+    #    return encr_session_key, encrypted_msg
 
+    def decrypt_msg(self, msg, session_key):
+        """ Расшифровать сообщение """
+        iv = msg[:16]
+        obj = AES.new(session_key, AES.MODE_CFB, iv)
+        decrypt_msg = obj.decrypt(msg)
+        return decrypt_msg[16:]
 
-    def run_cmd(self, cmd, msg):
+    def begin_register(self,id):
+        if id != -1:
+            self.socket.send(pickle.dumps(-1))
+        else:
+            d = {'first_name': '', 'second_name': ''}
+            self.socket.send(pickle.dumps((d, self.server_public_key)))
 
-        """ Выполнение запроса клиента """
-
-        if cmd == 'register_request':
-            d = {'first_name':'','second_name':''}
-            self.socket.send(pickle.dumps((d,self.server_public_key)))
-            cmd, new_dict, client_pub_key = pickle.loads(self.socket.recv())
-            if cmd == "finish_register":
-                new_dict['first_name'] = self.decryption_obj.decrypt(new_dict['first_name'])
-                new_dict['second_name'] = self.decryption_obj.decrypt(new_dict['second_name'])
-                new_dict['open_key'] = client_pub_key.exportKey()
-                # self.register_in_base()
-                self.socket.send(pickle.dumps('ok'))
-            print(new_dict)
+    def finish_register(self,msg,session_key,client_public_key):
+        decr_session_key = self.decryption_obj.decrypt(session_key)
+        msg['first_name'] = self.decrypt_msg(msg['first_name'], decr_session_key)
+        msg['second_name'] = self.decrypt_msg(msg['second_name'], decr_session_key)
+        msg['open_key'] = client_public_key.exportKey()
+        id = self.register_in_base(msg)
+        self.socket.send(pickle.dumps(('ok', id)))
+        print(self.data_of_constituency)
 
     def go(self, port):
-
         """ Главный цикл """
-
         self.socket.bind(port)
         while True:
-            try:
-                cmd, msg = pickle.loads(self.socket.recv())
-                self.run_cmd(cmd,msg)
-            except Exception:
-                self.socket.send(pickle.dumps('error'))
+            cmd, msg, session_key, client_public_key, id = pickle.loads(self.socket.recv())
+            if cmd == 'register_request':
+                self.begin_register(id)
+            if cmd == 'finish_register':
+                self.finish_register(msg,session_key,client_public_key)
 
 if __name__ == "__main__":
     server = ServerVote()
-    server.go('tcp://127.0.0.1:9090')
+    server.go('tcp://127.0.0.1:8090')
